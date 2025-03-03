460504c7e5d665dd6df330d6e669d9929f380048
aklema094 <aklemasultana094@gmail.com>
2025-03-03 21:31:26 +0600

    get current thread

---
+++ src/OOPS/MultithreadingInJava.java
@@ -0,0 +1,10 @@
+
+package OOPS;
+
+public class MultithreadingInJava {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(Thread.currentThread().getName());
+    }
+}

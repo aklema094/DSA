814364a31b52af11c48335cf9d5db406592302f5
aklema094 <aklemasultana094@gmail.com>
2025-03-09 16:39:06 +0600

    life cycle of thread

---
+++ src/OOPS/LifeCyleOfThread.java
@@ -0,0 +1,29 @@
+
+package OOPS;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LifeCyleOfThread extends Thread{
+    @Override
+    public void run(){
+        System.out.println("Running");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            System.out.println(ex);
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+
+        LifeCyleOfThread t1 = new LifeCyleOfThread();
+        System.out.println(t1.getState());
+        t1.start();
+        System.out.println(t1.getState());
+        Thread.sleep(100);
+        System.out.println(t1.getState());
+        t1.join();
+        System.out.println(t1.getState());
+    }
+    
+}
--- src/OOPS/ThreadsUsingthreadclass.java
+++ src/OOPS/ThreadsUsingthreadclass.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package OOPS;
 
-/**
- *
- * @author user
- */
 public class ThreadsUsingthreadclass {
     
     public static void main(String[] args) {

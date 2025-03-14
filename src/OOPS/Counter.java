fb2fc992523e2aaa67d4d1e7accdc725ff0bb57b
aklema094 <aklemasultana094@gmail.com>
2025-03-14 21:16:25 +0600

    sychronization

---
+++ src/OOPS/Counter.java
@@ -0,0 +1,15 @@
+
+package OOPS;
+
+public class Counter {
+    
+    public int count = 0;
+    
+    public  void increment(){
+        count++;
+    }
+    
+    public int getCount(){
+        return count;
+    }
+}
---
+++ src/OOPS/SyncronizationInJava.java
@@ -0,0 +1,23 @@
+
+package OOPS;
+
+public class SyncronizationInJava {
+    
+    public static void main(String[] args) {
+        Counter count = new Counter();
+        MyThread t1 = new MyThread(count);
+        MyThread t2 = new MyThread(count);
+        t1.start();
+        t2.start();
+        
+        try{
+            t1.join();
+            t2.join();
+        }catch(Exception e){
+            
+        }
+        System.out.println(count.getCount());
+    }
+   
+   
+}

521f0add6d720475bdfdf2f9b3408e8352011190
aklema094 <aklemasultana094@gmail.com>
2025-03-17 15:44:06 +0600

    Explicit lock

--- src/OOPS/BankAccount.java
+++ src/OOPS/BankAccount.java
@@ -1,28 +1,48 @@
 
 package OOPS;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 public class BankAccount {
      private int balance = 200;
+     private final Lock lock = new ReentrantLock();
     
-    public synchronized void withdraw(int amount){
+    public  void withdraw(int amount){
         
         System.out.println(Thread.currentThread().getName()+" attempting to withdraw " + amount);
         
-        if(amount<=balance){
+       try{
+           
+           if(lock.tryLock(1,TimeUnit.SECONDS)){
+                  if(amount<=balance){
             
-             System.out.println(Thread.currentThread().getName()+" proceeding with withdrawal " + amount);
-             try{
+                   try{
+                 
+                 System.out.println(Thread.currentThread().getName()+" proceeding with withdrawal " + amount);
                  Thread.sleep(3000);
+                  balance -= amount;
+              System.out.println(Thread.currentThread().getName()+" completed withdrawal " + amount);
              }catch(InterruptedException e){
                  
-             }
-             balance -= amount;
-              System.out.println(Thread.currentThread().getName()+" completed withdrawal " + amount);
+             }finally{
+                       lock.unlock();
+                   }
+            
             
         }else{
             System.out.println(Thread.currentThread().getName()+" insufficient balance");
         }
+               
+           }else{
+               System.out.println(Thread.currentThread().getName()+" could not acquire the lock! will try later. ");
+           }
+           
+       }catch(Exception e){
+           
+       }
     }
 }
 

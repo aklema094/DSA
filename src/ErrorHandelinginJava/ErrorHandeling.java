822071223db66ba77419797e3d9d755a822c0930
aklema094 <aklemasultana094@gmail.com>
2025-04-09 22:22:24 +0600

    NullPointerException

--- src/ErrorHandelinginJava/ErrorHandeling.java
+++ src/ErrorHandelinginJava/ErrorHandeling.java
@@ -3,9 +3,11 @@ package ErrorHandelinginJava;
 
 
 public class ErrorHandeling {
-    
+     
+       
     public static void main(String[] args) {
         
+     
         int[] numerators = {10,200,30,40};
         int[] denominators = {1,2,0,4};
         
@@ -14,15 +16,27 @@ public class ErrorHandeling {
             System.out.println(divide(numerators[i],denominators[i]));
             
         }
+       
         
     }
     
+    
     public static int divide(int a,int b){
+       
+        Test t = new Test();
+        t = null;
       try{
+           t.setValue(10);
             return a/b;
+            
+    }catch(NullPointerException e){
+          System.out.println(e);
+          return -1;
     }catch(ArithmeticException e){
           System.out.println(e);
         return -1;
     }
+          
 }
 }
+
---
+++ src/ErrorHandelinginJava/Test.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ErrorHandelinginJava;
+
+/**
+ *
+ * @author user
+ */
+public class Test {
+     int val;
+    public  int setValue(int i){
+       
+        this.val = i;
+        return val;
+    }
+    
+}
